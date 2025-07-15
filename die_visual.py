9cd8a23e72d3b16d0eddf7ee60cec7f8e336b7d4
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-15 15:40:18 +0300

    Further customize the chart axes

--- die_visual.py
+++ die_visual.py
@@ -1,21 +1,27 @@
 import plotly.express as px
+
 from die import Die
 
-# Create a D6
-die = Die()
+# Create two D6 dice.
+die_1 = Die()
+die_2 = Die()
+
 # Make some rolls, and store results in a list.
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-poss_results = range(1, die.num_sides+1)
+max_results = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_results+1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-fig = px.bar(x=poss_results, y=frequencies)
-fig.write_html("dice_plot.html")
+title = "Results of Rolling Two D6 Dice 1,000 Times."
+labels = {'x': 'Result', 'y': 'Frequency of Results.'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.write_html("die.html")
